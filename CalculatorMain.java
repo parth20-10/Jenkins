0664f50d438884545fe69f4e1b0741414c699f73
parth20-10 <Parth.Masal@wissen.com>
2025-02-13 15:48:43 +0530

    1st change

--- CalculatorMain.java
+++ CalculatorMain.java
@@ -17,7 +17,7 @@ public class CalculatorMain
 {
 	public static void main(String args[])
 	{
-		System.out.println("Calculator Operations......");
+		System.out.println("Calculator Operations changes......");
  
 		Calculator c1 = new Calculator();
 		c1.add(10, 20);
